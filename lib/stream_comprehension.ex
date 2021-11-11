d15df0f6ebf8fb479cff6d40cc345f83a0ada762
Brett Beatty <brettbeatty@gmail.com>
2021-11-10 21:25:30 -0700

    Draft working implementation for map/filter ops

--- lib/stream_comprehension.ex
+++ lib/stream_comprehension.ex
@@ -1,6 +1,12 @@
 defmodule StreamComprehension do
-  @typep reason() :: :start_with_generator | {:usage, arity :: 1..2}
-  @typep state() :: %{ast: Macro.t(), uniq: boolean()}
+  @typep reason() ::
+           :invalid_opt
+           | :invalid_opts
+           | :invalid_uniq
+           | :missing_do
+           | :start_with_generator
+           | {:usage, arity :: 1..2}
+  @typep acc() :: %{ast: Macro.t(), env: Macro.Env.t(), uniq: boolean()}
 
   defmacro stream(comprehension)
 
@@ -32,14 +38,86 @@ defmodule StreamComprehension do
     raise compile_error({:usage, _arity = 2}, __CALLER__)
   end
 
-  @spec build_stream([Macro.t()], Macro.Env.t()) :: Macro.t()
-  defp build_stream(_args, _env) do
-    raise "not implemented"
-  end
-
   @spec starts_with_generator?([Macro.t()]) :: boolean()
   defp starts_with_generator?(args) do
     match?([{:<-, _meta, [_match, _enumerable]} | _args], args)
+  end
+
+  @spec build_stream([Macro.t()], Macro.Env.t()) :: Macro.t()
+  defp build_stream(args, env) do
+    with [opts | remaining] when is_list(opts) <- args,
+         {[do: block], new_opts} <- Keyword.split(opts, [:do]) do
+      build_stream([new_opts | remaining], block, env)
+    else
+      _ ->
+        raise compile_error(:missing_do, env)
+    end
+  end
+
+  @spec build_stream([Macro.t()], Macro.t(), Macro.Env.t()) :: Macro.t()
+  defp build_stream(args, block, env) do
+    acc = %{ast: [block], env: env, uniq: false}
+
+    %{ast: ast, uniq: uniq} = Enum.reduce(args, acc, &apply_arg/2)
+
+    if uniq do
+      quote do
+        Stream.uniq(unquote(ast))
+      end
+    else
+      ast
+    end
+  end
+
+  @spec apply_arg(Macro.t(), acc()) :: acc()
+  defp apply_arg(arg, acc)
+
+  defp apply_arg({:<-, _meta, [match, enumerable]}, acc) do
+    ast =
+      quote generated: true do
+        Stream.flat_map(unquote(enumerable), fn
+          unquote(match) -> unquote(acc.ast)
+          _ -> []
+        end)
+      end
+
+    %{acc | ast: ast}
+  end
+
+  defp apply_arg(opts, acc) when is_list(opts) do
+    Enum.reduce(opts, acc, &apply_opt/2)
+  end
+
+  defp apply_arg(condition, acc) do
+    ast =
+      quote do
+        if unquote(condition) do
+          unquote(acc.ast)
+        else
+          []
+        end
+      end
+
+    %{acc | ast: ast}
+  end
+
+  @spec apply_opt({:uniq, boolean()} | term(), acc()) :: acc() | no_return()
+  defp apply_opt(opt, acc)
+
+  defp apply_opt({:uniq, uniq}, acc) do
+    if is_boolean(uniq) do
+      %{acc | uniq: uniq}
+    else
+      raise compile_error({:invalid_uniq, uniq}, acc.env)
+    end
+  end
+
+  defp apply_opt({opt, _value}, acc) do
+    raise compile_error({:invalid_opt, opt}, acc.env)
+  end
+
+  defp apply_opt(_opt, acc) do
+    raise compile_error(:invalid_opts, acc.env)
   end
 
   @spec compile_error(reason(), Macro.Env.t()) :: Exception.t()
@@ -49,6 +127,22 @@ defmodule StreamComprehension do
 
   @spec describe(reason()) :: String.t()
   defp describe(reason)
+
+  defp describe({:invalid_opt, opt}) do
+    "unsupported option #{inspect(opt)} given to for"
+  end
+
+  defp describe(:invalid_opts) do
+    "for comprehension options must be in keyword list"
+  end
+
+  defp describe({:invalid_uniq, uniq}) do
+    ":uniq option for comprehensions only accepts a boolean, got: #{inspect(uniq)}"
+  end
+
+  defp describe(:missing_do) do
+    ~S(missing :do option in "for")
+  end
 
   defp describe(:start_with_generator) do
     "for comprehensions must start with a generator"
--- test/stream_comprehension_test.exs
+++ test/stream_comprehension_test.exs
@@ -16,32 +16,246 @@ defmodule StreamComprehensionTest do
   end
 
   describe "stream/1" do
+    import StreamComprehension, only: [stream: 1]
+
+    test "success: returns an enumerable" do
+      my_stream = stream for x <- ?a..?c, do: x
+
+      assert Enum.to_list(my_stream) == 'abc'
+    end
+
+    test "success: supports map operations" do
+      my_stream = stream for x <- ?a..?c, do: x + 1
+
+      assert Enum.to_list(my_stream) == 'bcd'
+    end
+
+    test "success: supports filter operations" do
+      my_stream = stream for x <- ?a..?c, x != ?b, do: x
+
+      assert Enum.to_list(my_stream) == 'ac'
+    end
+
+    test "success: supports match filtering" do
+      my_stream = stream for {:ok, x} <- [ok: ?a, error: ?b, ok: ?c], do: x
+
+      assert Enum.to_list(my_stream) == 'ac'
+    end
+
+    test "success: not :uniq by default" do
+      my_stream = stream for x <- 'aabc', do: x + 1
+
+      assert Enum.to_list(my_stream) == 'bbcd'
+    end
+
+    test "success: supports :uniq option" do
+      my_stream = stream for x <- 'aabc', uniq: true, do: x + 1
+
+      assert Enum.to_list(my_stream) == 'bcd'
+    end
+
+    test "success: supports multiple generators" do
+      my_stream = stream for x <- ?a..?c, x != ?b, y <- ?d..?f, do: <<x, y>>
+
+      assert Enum.to_list(my_stream) == ["ad", "ae", "af", "cd", "ce", "cf"]
+    end
+
+    test "success: does not evaluate skipped expressions" do
+      my_stream = stream for {:ok, x} <- [ok: ?a, error: ?b, ok: ?c], x != ?c, do: send(self(), x)
+
+      Stream.run(my_stream)
+
+      assert_received ?a
+      refute_received ?b
+      refute_received ?c
+    end
+
+    test "success: operations performed lazily" do
+      my_stream =
+        stream for x <- ?a..?c, x != ?b, y <- ?d..?f, y == ?f, do: send(self(), <<x, y>>)
+
+      refute_received "af"
+      refute_received "cf"
+
+      Stream.run(my_stream)
+
+      assert_received "af"
+      assert_received "cf"
+    end
+
     test "error: fails if not passed a for comprehension" do
       assert_compile_error "StreamComprehension.stream/1 can only be passed a for comprehension" do
-        stream []
+        stream x
       end
     end
 
     test "error: fails if comprehension does not start with generator" do
       assert_compile_error "for comprehensions must start with a generator" do
-        stream for 1..3, do: []
+        stream for x, do: x
+      end
+    end
+
+    test "error: fails if no :do block" do
+      assert_compile_error ~S(missing :do option in "for") do
+        stream for x <- 1..3
+      end
+    end
+
+    test "error: fails if :uniq option not boolean" do
+      assert_compile_error ":uniq option for comprehensions only accepts a boolean, got: :yes" do
+        stream for x <- 1..3, uniq: :yes, do: x
+      end
+    end
+
+    test "error: fails if unsupported option passed" do
+      assert_compile_error "unsupported option :into given to for" do
+        stream for x <- 1..3, into: MapSet.new(), do: x
+      end
+    end
+
+    test "error: fails if options not in keyword list" do
+      assert_compile_error "for comprehension options must be in keyword list" do
+        stream for x <- 1..3, [:uniq], do: x
       end
     end
   end
 
   describe "stream/2" do
+    import StreamComprehension, only: [stream: 2]
+
+    test "success: returns an enumerable" do
+      my_stream =
+        stream for x <- ?a..?c do
+          x
+        end
+
+      assert Enum.to_list(my_stream) == 'abc'
+    end
+
+    test "success: supports map operations" do
+      my_stream =
+        stream for x <- ?a..?c do
+          x + 1
+        end
+
+      assert Enum.to_list(my_stream) == 'bcd'
+    end
+
+    test "success: supports filter operations" do
+      my_stream =
+        stream for x <- ?a..?c, x != ?b do
+          x
+        end
+
+      assert Enum.to_list(my_stream) == 'ac'
+    end
+
+    test "success: supports match filtering" do
+      my_stream =
+        stream for {:ok, x} <- [ok: ?a, error: ?b, ok: ?c] do
+          x
+        end
+
+      assert Enum.to_list(my_stream) == 'ac'
+    end
+
+    test "success: not :uniq by default" do
+      my_stream =
+        stream for x <- 'aabc' do
+          x + 1
+        end
+
+      assert Enum.to_list(my_stream) == 'bbcd'
+    end
+
+    test "success: supports :uniq option" do
+      my_stream =
+        stream for x <- 'aabc', uniq: true do
+          x + 1
+        end
+
+      assert Enum.to_list(my_stream) == 'bcd'
+    end
+
+    test "success: supports multiple generators" do
+      my_stream =
+        stream for x <- ?a..?c, x != ?b, y <- ?d..?f do
+          <<x, y>>
+        end
+
+      assert Enum.to_list(my_stream) == ["ad", "ae", "af", "cd", "ce", "cf"]
+    end
+
+    test "success: does not evaluate skipped expressions" do
+      my_stream =
+        stream for {:ok, x} <- [ok: ?a, error: ?b, ok: ?c], x != ?c do
+          send(self(), x)
+        end
+
+      Stream.run(my_stream)
+
+      assert_received ?a
+      refute_received ?b
+      refute_received ?c
+    end
+
+    test "success: operations performed lazily" do
+      my_stream =
+        stream for x <- ?a..?c, x != ?b, y <- ?d..?f, y == ?f do
+          send(self(), <<x, y>>)
+        end
+
+      refute_received "af"
+      refute_received "cf"
+
+      Stream.run(my_stream)
+
+      assert_received "af"
+      assert_received "cf"
+    end
+
     test "error: fails if not passed a for comprehension" do
       assert_compile_error "StreamComprehension.stream/2 can only be passed a for comprehension" do
-        stream 1..3 do
-          []
+        stream x do
+          x
         end
       end
     end
 
     test "error: fails if comprehension does not start with generator" do
       assert_compile_error "for comprehensions must start with a generator" do
-        stream for 1..3 do
-          []
+        stream for x do
+          x
+        end
+      end
+    end
+
+    test "error: fails if no :do block" do
+      assert_compile_error ~S(missing :do option in "for") do
+        stream(for(x <- 1..3), [])
+      end
+    end
+
+    test "error: fails if :uniq option not boolean" do
+      assert_compile_error ":uniq option for comprehensions only accepts a boolean, got: :yes" do
+        stream for x <- 1..3, uniq: :yes do
+          x
+        end
+      end
+    end
+
+    test "error: fails if unsupported option passed" do
+      assert_compile_error "unsupported option :into given to for" do
+        stream for x <- 1..3, into: MapSet.new() do
+          x
+        end
+      end
+    end
+
+    test "error: fails if options not in keyword list" do
+      assert_compile_error "for comprehension options must be in keyword list" do
+        stream for x <- 1..3, [:uniq] do
+          x
         end
       end
     end
