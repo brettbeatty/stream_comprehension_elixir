b6457ad83eef523185843adad714bab864721e36
Brett Beatty <brettbeatty@gmail.com>
2021-11-10 18:56:57 -0700

    Draft macro contracts

--- lib/stream_comprehension.ex
+++ lib/stream_comprehension.ex
@@ -1,18 +1,60 @@
 defmodule StreamComprehension do
-  @moduledoc """
-  Documentation for `StreamComprehension`.
-  """
+  @typep reason() :: :start_with_generator | {:usage, arity :: 1..2}
+  @typep state() :: %{ast: Macro.t(), uniq: boolean()}
 
-  @doc """
-  Hello world.
+  defmacro stream(comprehension)
 
-  ## Examples
+  defmacro stream({:for, _meta, args}) when is_list(args) do
+    if starts_with_generator?(args) do
+      args
+      |> Enum.reverse()
+      |> build_stream(__CALLER__)
+    else
+      raise compile_error(:start_with_generator, __CALLER__)
+    end
+  end
 
-      iex> StreamComprehension.hello()
-      :world
+  defmacro stream(_comprehension) do
+    raise compile_error({:usage, _arity = 1}, __CALLER__)
+  end
 
-  """
-  def hello do
-    :world
+  defmacro stream(comprehension, block)
+
+  defmacro stream({:for, _meta, args}, opts) when is_list(args) and is_list(opts) do
+    if starts_with_generator?(args) do
+      build_stream([opts | Enum.reverse(args)], __CALLER__)
+    else
+      raise compile_error(:start_with_generator, __CALLER__)
+    end
+  end
+
+  defmacro stream(_comprehension, _block) do
+    raise compile_error({:usage, _arity = 2}, __CALLER__)
+  end
+
+  @spec build_stream([Macro.t()], Macro.Env.t()) :: Macro.t()
+  defp build_stream(_args, _env) do
+    raise "not implemented"
+  end
+
+  @spec starts_with_generator?([Macro.t()]) :: boolean()
+  defp starts_with_generator?(args) do
+    match?([{:<-, _meta, [_match, _enumerable]} | _args], args)
+  end
+
+  @spec compile_error(reason(), Macro.Env.t()) :: Exception.t()
+  defp compile_error(reason, env) do
+    CompileError.exception(description: describe(reason), file: env.file, line: env.line)
+  end
+
+  @spec describe(reason()) :: String.t()
+  defp describe(reason)
+
+  defp describe(:start_with_generator) do
+    "for comprehensions must start with a generator"
+  end
+
+  defp describe({:usage, arity}) do
+    "StreamComprehension.stream/#{arity} can only be passed a for comprehension"
   end
 end
--- test/stream_comprehension_test.exs
+++ test/stream_comprehension_test.exs
@@ -1,8 +1,49 @@
 defmodule StreamComprehensionTest do
-  use ExUnit.Case
-  doctest StreamComprehension
+  use ExUnit.Case, async: true
 
-  test "greets the world" do
-    assert StreamComprehension.hello() == :world
+  defmacrop assert_compile_error(message, do: block) do
+    quote bind_quoted: [message: message, block: Macro.escape(block)] do
+      new_block =
+        quote do
+          import StreamComprehension
+
+          unquote(block)
+        end
+
+      exception = assert_raise CompileError, fn -> Code.eval_quoted(new_block) end
+      assert Exception.message(exception) =~ message
+    end
+  end
+
+  describe "stream/1" do
+    test "error: fails if not passed a for comprehension" do
+      assert_compile_error "StreamComprehension.stream/1 can only be passed a for comprehension" do
+        stream []
+      end
+    end
+
+    test "error: fails if comprehension does not start with generator" do
+      assert_compile_error "for comprehensions must start with a generator" do
+        stream for 1..3, do: []
+      end
+    end
+  end
+
+  describe "stream/2" do
+    test "error: fails if not passed a for comprehension" do
+      assert_compile_error "StreamComprehension.stream/2 can only be passed a for comprehension" do
+        stream 1..3 do
+          []
+        end
+      end
+    end
+
+    test "error: fails if comprehension does not start with generator" do
+      assert_compile_error "for comprehensions must start with a generator" do
+        stream for 1..3 do
+          []
+        end
+      end
+    end
   end
 end
